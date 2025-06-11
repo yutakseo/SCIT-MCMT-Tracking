353f9302f88edfc5cc360a85613482e8b542ccdc
Yutak Seo <yts@smartinside.ai>
2025-06-11 13:08:21 +0900

    25.06.11

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-pip install -r requirements.txt
-python setup.py develop
-pip install cython
-pip install 'git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI'
-pip install cython_bbox
-pip install mmcv-full==1.7.1 -f https://download.openmmlab.com/mmcv/dist/cu113/torch1.11.0/index.html
-pip install -v -e .
